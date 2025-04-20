a5c93585d559859795957f3429f7f7bc9177ebf7
Sheshbabu <sheshbabu@gmail.com>
2025-04-20 16:17:36 +0800

    Updated mobile navbar to show search menu

--- commons/components/MobileNavbar.jsx
+++ commons/components/MobileNavbar.jsx
@@ -1,8 +1,13 @@
-import { h } from '../../assets/preact.esm.js';
+import { h, render } from "../../assets/preact.esm.js"
 import Link from './Link.jsx';
+import SearchMenu from "../../features/search/SearchMenu.jsx";
 import { NotesIcon, SearchIcon, NewIcon } from "./Icon.jsx"
 
 export default function MobileNavbar() {
+  function handleSearchClick() {
+    render(<SearchMenu />, document.querySelector('.modal-root'));
+  }
+
   return (
     <div className="mobile-navbar-container">
       <div className="mobile-navbar">
@@ -10,10 +15,10 @@ export default function MobileNavbar() {
           <NotesIcon />
           Notes
         </Link>
-        <Link className="mobile-navbar-button" to="/notes/">
+        <div className="mobile-navbar-button" onClick={handleSearchClick}>
           <SearchIcon />
           Search
-        </Link>
+        </div>
         <Link className="mobile-navbar-button" to="/notes/new" shouldPreserveSearchParams>
           <NewIcon />
           New
