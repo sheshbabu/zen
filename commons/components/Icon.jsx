f44f66509f80ad7b4c047e2ba04edf11f4758abd
Sheshbabu <sheshbabu@gmail.com>
2025-04-23 18:30:57 +0800

    Added editor menu icon

--- commons/components/Icon.jsx
+++ commons/components/Icon.jsx
@@ -150,3 +150,13 @@ export function RemoveIcon({ onClick }) {
     </svg>
   );
 }
+
+export function EllipsisIcon({ onClick }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" class="lucide lucide-ellipsis-icon lucide-ellipsis">
+      <circle cx="12" cy="12" r="1" />
+      <circle cx="19" cy="12" r="1" />
+      <circle cx="5" cy="12" r="1" />
+    </svg>
+  );
+}
