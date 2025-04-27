6034d2365373cac4f393fe48eb7f5dae34c055d9
Sheshbabu <sheshbabu@gmail.com>
2025-04-27 19:35:11 +0800

    Made note delete modal visually consistent

--- features/notes/NoteDeleteModal.jsx
+++ features/notes/NoteDeleteModal.jsx
@@ -1,11 +1,19 @@
 import { h } from "../../assets/preact.esm.js"
+import { CloseIcon } from "../../commons/components/Icon.jsx";
 
 export default function NoteDeleteModal({ onDeleteClick, onCloseClick }) {
+  function handleBackdropClick(e) {
+    if (e.target.classList.contains("modal-backdrop-container")) {
+      onCloseClick();
+    }
+  }
+
   return (
-    <div className="modal-backdrop-container is-centered">
+    <div className="modal-backdrop-container is-centered" onClick={handleBackdropClick}>
       <div className="modal-content-container note-delete-modal">
         <div className="modal-header">
           <h3 className="modal-title">Delete Note</h3>
+          <CloseIcon className="notes-editor-toolbar-button-close" onClick={onCloseClick} />
         </div>
         <div className="modal-content">
           <p>This note will be moved to the Trash and <b>permanently deleted</b> after 30 days.</p>
