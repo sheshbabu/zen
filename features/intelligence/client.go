72ea75bab74b2d8a3a89324abe01e9e47ccc45b9
Sheshbabu <sheshbabu@gmail.com>
2025-10-03 09:02:23 +0800

    Increased client timeout

--- features/intelligence/client.go
+++ features/intelligence/client.go
@@ -13,7 +13,7 @@ import (
 var baseURL string
 var httpClient *http.Client
 
-const CLIENT_TIMEOUT = 5 * time.Second
+const CLIENT_TIMEOUT = 60 * time.Second
 
 type EmbedNoteRequest struct {
 	Title     string   `json:"title"`
